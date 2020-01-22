d6252bbd00c239a1df1cbe10514afda3b58982db
ssyup4259 <30275948+ssyup4259@users.noreply.github.com>
2020-01-22 19:45:57 +0900

    오늘은 여기까지

--- src/Solution_9280_진용이네주차타워.java
+++ src/Solution_9280_진용이네주차타워.java
@@ -6,29 +6,31 @@ public class Solution_9280_진용이네주차타워 {
 	static int n, m, r[], w[], sum;
 	static boolean[] visit;
 	static int[] where;
-	static Queue<Integer> que = new LinkedList<>();
+	static Queue<Integer> que;
 
 	static void in(int now) {
 		int num = 0;
-		if (que.isEmpty()) {
-			num = now;
-		} else {
+		if (que.isEmpty()) { // 대기차가 없다면 그 차 기준
+			num = now; //
+		} else { // 대기 하는 차가 있다면 대기하고 있던 차 기준
 			num = que.peek();
+			que.add(now);
 		}
 
 		boolean flag = false;
 		for (int i = 0; i < n; i++) {
 			if (!visit[i]) {
 				visit[i] = true;
-				where[num] = i;
+				where[num] = i; // num 차를 i번 주차장에 주차 했다고 표시
 				sum += (r[i] * w[num]);
 				que.poll();
 				flag = true;
 				break;
 			}
 		}
-		if (!flag) {
-			que.add(now);
+
+		if (!flag && que.isEmpty()) { // 주차장 꽉찼다면
+			que.add(num);
 		}
 	}
 
@@ -58,6 +60,7 @@ public class Solution_9280_진용이네주차타워 {
 			}
 			sum = 0;
 			visit = new boolean[n];
+			que = new LinkedList<>();
 			for (int i = 0; i < 2 * m; i++) {
 				int x = sc.nextInt();
 				if (x > 0) {
