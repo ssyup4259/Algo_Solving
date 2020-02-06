7b2168ef2ef80fbaf468362870433a78f9806139
ssyup4259 <ssyup8717@gmail.com>
2020-02-06 19:15:45 +0900

    solve 1330

---
+++ src/OTHER/Main_백준_1330_두수비교하기.java
@@ -0,0 +1,20 @@
+package OTHER;
+
+import java.util.Scanner;
+
+public class Main_백준_1330_두수비교하기 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+
+		if (A > B) {
+			System.out.println(">");
+		} else if (A < B) {
+			System.out.println("<");
+		} else {
+			System.out.println("==");
+		}
+	}
+}
