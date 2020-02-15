b00989f7ff9949098a0fa5ab01862854ce0b2b3f
ssyup4259 <ssyup8717@gmail.com>
2020-02-15 19:28:54 +0900

    solve 10960

---
+++ src/OTHER/Main_백준_10950_A더하기B3.java
@@ -0,0 +1,17 @@
+package OTHER;
+
+import java.util.Scanner;
+
+public class Main_백준_10950_A더하기B3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			System.out.println(A + B);
+		}
+	}
+
+}
