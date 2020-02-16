6d462bc21517ddda80ed69ba8e80b35187577ffb
ssyup4259 <ssyup8717@gmail.com>
2020-02-16 19:31:35 +0900

    solve 10951

---
+++ src/OTHER/Main_백준_10951_A더하기B4.java
@@ -0,0 +1,17 @@
+package OTHER;
+
+import java.util.Scanner;
+
+public class Main_백준_10951_A더하기B4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			System.out.println(A + B);
+		}
+
+	}
+
+}
