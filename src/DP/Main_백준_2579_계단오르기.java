37920473a48053196ac30c5572db8c79948caa05
ssyup4259 <ssyup8717@gmail.com>
2020-02-19 21:57:23 +0900

    problem solving with dp

--- src/DP/Main_백준_2579_계단오르기.java
+++ src/DP/Main_백준_2579_계단오르기.java
@@ -1,5 +1,6 @@
 package DP;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main_백준_2579_계단오르기 {
@@ -7,29 +8,18 @@ public class Main_백준_2579_계단오르기 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt(); // 계단의 개수 최대 300개
-		int[] score = new int[N]; // 첨수 최대 10000
-		int[] memo = new int[N];
-		memo[0] = score[0];
-		memo[1] = score[0] + score[1];
-		int cnt = 0; // 3 번 연속한 계단인지 검사
+		int[] score = new int[N + 1]; // 첨수 최대 10000
+		int[] memo = new int[N + 1];
 		int max = 0;
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			score[i] = sc.nextInt();
-			if (i >= 2) {
-				cnt++;
-				int a = memo[i - 2];
-				int b = memo[i - 1];
-				if (a < b && cnt < 2) {
-					memo[i] = b + score[i];
-					cnt++;
-				} else {
-					memo[i] = a + score[i];
-					cnt = 1;
-				}
-			}
-			max = max < memo[i] ? memo[i] : max;
 		}
-		System.out.println(max);
+		memo[1] = score[1];
+		memo[2] = score[1] + score[2];
+		for (int i = 3; i <= N; i++) {
+			memo[i] = Math.max(score[i - 1] + memo[i - 3], memo[i - 2]) + score[i];
+		}
+		System.out.println(memo[N]);
 	}
 
 }
