730c247dba73207821c25d79902deb2f4081de0f
ssyup4259 <ssyup8717@gmail.com>
2020-03-21 19:43:55 +0900

    solve 2750

---
+++ src/OTHER/Main_백준_2750_수정렬하기.java
@@ -0,0 +1,23 @@
+package OTHER;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main_백준_2750_수정렬하기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		for (int i = 0; i < N; i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
