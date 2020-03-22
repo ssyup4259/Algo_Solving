ac4499ebceb168256d7b34e16a4ee2fc9448df8c
ssyup4259 <ssyup8717@gmail.com>
2020-03-22 15:21:05 +0900

    update 2751

--- src/OTHER/Main_백준_2751_수정렬하기2.java
+++ src/OTHER/Main_백준_2751_수정렬하기2.java
@@ -11,10 +11,12 @@ public class Main_백준_2751_수정렬하기2 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[2000001];
+
 		for (int i = 0; i < N; i++) {
 			int num = Integer.parseInt(br.readLine());
 			arr[num + 1000000] = 1;
 		}
+
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] == 1) {
 				System.out.println(i - 1000000);
