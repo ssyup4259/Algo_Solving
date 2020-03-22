1cfdf289245bd0811f1c56c51dd88530e5b59c52
ssyup4259 <ssyup8717@gmail.com>
2020-03-22 15:20:34 +0900

    refactore 2751

--- src/OTHER/Main_백준_2751_수정렬하기2.java
+++ src/OTHER/Main_백준_2751_수정렬하기2.java
@@ -4,20 +4,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main_백준_2751_수정렬하기2 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
+		int[] arr = new int[2000001];
 		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+			int num = Integer.parseInt(br.readLine());
+			arr[num + 1000000] = 1;
 		}
-		Arrays.sort(arr);
-		for (int i = 0; i < N; i++) {
-			System.out.println(arr[i]);
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 1) {
+				System.out.println(i - 1000000);
+			}
 		}
 	}
 
