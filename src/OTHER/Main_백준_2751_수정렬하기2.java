480e51f9abbdc016620b5dc753a0d33572422201
ssyup4259 <ssyup8717@gmail.com>
2020-03-22 20:06:22 +0900

    solve 2751

---
+++ src/OTHER/Main_백준_2751_수정렬하기2.java
@@ -0,0 +1,21 @@
+package OTHER;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main_백준_2751_수정렬하기2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		for (int i = 0; i < N; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
