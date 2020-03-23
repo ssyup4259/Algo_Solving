e3cc5e37219515075e59c2f9cac5621eff848c01
ssyup4259 <ssyup8717@gmail.com>
2020-03-23 16:17:47 +0900

    sovle 1920

--- src/OTHER/Main_백준_1920_수찾기.java
+++ src/OTHER/Main_백준_1920_수찾기.java
@@ -1,6 +1,7 @@
 package OTHER;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main_백준_1920_수찾기 {
@@ -8,34 +9,17 @@ public class Main_백준_1920_수찾기 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		int[] arr = new int[N];
+		HashSet<Integer> hs = new HashSet<>();
 		for (int i = 0; i < N; i++) {
-			arr[i] = sc.nextInt();
+			int num = sc.nextInt();
+			hs.add(num);
 		}
-		Arrays.sort(arr);
 		int M = sc.nextInt();
-		int[] arr2 = new int[M];
 		for (int i = 0; i < M; i++) {
-			arr2[i] = sc.nextInt();
-		}
-		Arrays.sort(arr2);
-		System.out.println(Arrays.toString(arr));
-		System.out.println(Arrays.toString(arr2));
-		int flag = 0;
-		for (int i = 0; i < M; i++) {
-			int num = arr2[i];
-			boolean check = false;
-			while (flag < N && arr[flag] <= num) {
-				if (arr[flag] == num) {
-					check = true;
-					break;
-				}
-				flag++;
-			}
-			if (check) {
-				flag++;
+			int num = sc.nextInt();
+			if (hs.contains(Integer.valueOf(num))) {
 				System.out.println(1);
-			} else {
+			}else {
 				System.out.println(0);
 			}
 		}
