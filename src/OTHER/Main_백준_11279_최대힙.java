d581ec5c55fd270fc15cb59336e8ae651152ad24
ssyup4259 <ssyup8717@gmail.com>
2020-04-01 23:55:03 +0900

    solve 11279

---
+++ src/OTHER/Main_백준_11279_최대힙.java
@@ -0,0 +1,32 @@
+package OTHER;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main_백준_11279_최대힙 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return Integer.compare(o2, o1);
+			}
+		});
+		for (int i = 0; i < N; i++) {
+			int num = sc.nextInt();
+			if (num == 0) {
+				if (pq.isEmpty()) {
+					System.out.println(0);
+				} else {
+					System.out.println(pq.poll());
+				}
+			} else {
+				pq.add(num);
+			}
+		}
+	}
+
+}
