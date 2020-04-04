9ebcd718e879f16797ce55c9eda549b2f4e1f021
ssyup4259 <ssyup8717@gmail.com>
2020-04-04 23:16:15 +0900

    solve 11726

--- src/DP/Main_백준_11726_2xn타일링.java
+++ src/DP/Main_백준_11726_2xn타일링.java
@@ -7,7 +7,7 @@ public class Main_백준_11726_2xn타일링 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		int[] dp = new int[N + 1];
+		int[] dp = new int[1001];
 		dp[0] = 0;
 		dp[1] = 1;
 		dp[2] = 2;
@@ -15,7 +15,5 @@ public class Main_백준_11726_2xn타일링 {
 			dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
 		}
 		System.out.println(dp[N]);
-
 	}
-
 }
