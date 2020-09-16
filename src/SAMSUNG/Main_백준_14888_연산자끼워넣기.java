5aee9d94500a1f02a519eec49ee59ca29495d84d
ssyup4259 <ssyup8717@gmail.com>
2020-09-17 00:12:54 +0900

    solve algo

---
+++ src/SAMSUNG/Main_백준_14888_연산자끼워넣기.java
@@ -0,0 +1,90 @@
+package SAMSUNG;
+
+import java.util.Scanner;
+
+public class Main_백준_14888_연산자끼워넣기 {
+	static int n;
+	static int[] arr;
+	static int[] oper;
+	static int min, max;
+
+	static int[] swap(int[] arr, int a, int b) {
+		int temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
+		return arr;
+	}
+
+	static int sub(int a, int b, int o) {
+		int answer = 0;
+		if (o == 0) {
+			answer = a + b;
+		} else if (o == 1) {
+			answer = a - b;
+		} else if (o == 2) {
+			answer = a * b;
+		} else if (o == 3) {
+			if (a < 0) {
+				a = -a;
+				answer = a / b;
+				answer = -answer;
+			} else {
+				answer = a / b;
+			}
+		}
+		return answer;
+	}
+
+	static void solve(int[] order, int cnt) {
+		if (cnt == n - 1) {
+			return;
+		}
+		int result = 0;
+		result = result + arr[0];
+		for (int i = 1; i < n; i++) {
+			result = sub(result, arr[i], order[i - 1]);
+			// System.out.println("result: " + result + " arr[i]: " + arr[i] + " order[i-1]:
+			// " + order[i - 1]);
+		}
+		// System.out.println("result: " + result);
+		if (min > result) {
+			min = result;
+		}
+		if (max < result) {
+			max = result;
+		}
+		for (int i = cnt; i <= n - 2; i++) {
+			order = swap(order, cnt, i);
+			solve(order, cnt + 1);
+			order = swap(order, i, cnt);
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		oper = new int[4]; // +,-,*,% 의 갯수
+		int[] order = new int[n - 1]; // 연산자 정렬용
+		int flag = 0;
+		for (int i = 0; i < 4; i++) {
+			oper[i] = sc.nextInt();
+			if (oper[i] != 0) {
+				for (int j = flag; j < flag + oper[i]; j++) {
+					order[j] = i;
+				}
+				flag = flag + oper[i];
+			}
+		}
+		min = 1000000000;
+		max = -1000000000;
+		solve(order, 0);
+		System.out.println(max);
+		System.out.println(min);
+
+	}
+
+}
---
+++ src/SIMULATION/Main_백준_14503_로봇청소기.java
@@ -0,0 +1,99 @@
+package SIMULATION;
+
+import java.util.Scanner;
+
+public class Main_백준_14503_로봇청소기 {
+	static int N, M, R, C, D;
+	static int[][] map;
+	static boolean[][] visit;
+	// up 0
+	static final int[][] dir = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+	static int cnt, dCnt;
+
+	static boolean isRange(int a, int b) { // map범위 안에 있는지 확인
+		boolean flag = false;
+		if (a >= 1 && a < N - 1 && b >= 1 && b < M - 1) {
+			flag = true;
+		}
+		return flag;
+	}
+
+	static void next(int r, int c, int d) {
+		int nextR = r;
+		int nextC = c;
+		int nextD;
+		if (d == 0) {
+			nextD = 3;
+		} else {
+			nextD = d - 1;
+		}
+		nextR = r + dir[nextD][0];
+		nextC = c + dir[nextD][1];
+		// 네 방향 모두 청소가 이미 되있거나 벽인 경우
+		if (dCnt == 4) {
+			if (d == 0) {
+				nextD = 2;
+			} else if (d == 1) {
+				nextD = 3;
+			} else if (d == 2) {
+				nextD = 0;
+			} else {
+				nextD = 1;
+			}
+			nextR = r + dir[nextD][0];
+			nextC = c + dir[nextD][1];
+			// 뒤로 갈 수 있다면 후진, 없다면 작동중지
+			if (isRange(nextR, nextC) && map[nextR][nextC] == 0) {
+				dCnt = 0;
+				next(nextR, nextC, d);
+				return;
+			} else if (!isRange(nextR, nextC) || map[nextR][nextC] == 1) {
+				return;
+			}
+		} else {
+			if (!isRange(nextR, nextC) || map[nextR][nextC] == 1) {
+				dCnt++;
+				next(r, c, nextD);
+				return;
+			}
+			// 다음 방향이 청소하지 않은 공간이라면
+			if (isRange(nextR, nextC) && map[nextR][nextC] == 0 && !visit[nextR][nextC]) {
+				dCnt = 0;
+				cnt++;
+				visit[nextR][nextC] = true;
+				next(nextR, nextC, nextD); // 다음 청소
+				return;
+			}
+			// 다음 방향이 청소할 공간이 없다면(벽이거나 청소를 하였다면)
+			if (isRange(nextR, nextC) && (map[nextR][nextC] == 1 || visit[nextR][nextC])) {
+				dCnt++;
+				next(r, c, nextD);
+				return;
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		R = sc.nextInt();
+		C = sc.nextInt();
+		D = sc.nextInt();
+		map = new int[N][M];
+		visit = new boolean[N][M];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				visit[i][j] = false;
+				map[i][j] = sc.nextInt();
+			}
+		}
+		cnt = 1;
+		dCnt = 0;
+		visit[R][C] = true;
+		next(R, C, D);
+		System.out.println(cnt);
+	}
+
+}
